60bea62b3b3a109a3617c379b27ba810482fd930
xmaiying <58544027+xmaiying@users.noreply.github.com>
2023-06-30 17:55:09 -0400

    formulaireflexible
    le formulaire bouge selon le question qui se montre mais, je n'arrive pas a diminuer la hauteur du formulaire quand le nombre de reclammations diminue

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    var isValide = false; 
 
     var select = document.getElementById("age");
     for(var i = 16; i <= 100; i++) {
@@ -68,18 +69,35 @@ function handleNombreReclamationsChange() {
     errorElement.style.color = "red";
     errorElement.style.backgroundColor = "white";
 
+    // MODIFICAITON DE LA TAILLE DU FORMULAIRE
+    var tailleFormulaire = document.getElementById('formeassurance');
+    var hauteurMtn = tailleFormulaire.clientHeight;
+    
+
     if (nombreReclamations > 4) {
+        errorElement.style.marginBottom = '20px';
         errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour ce profil de client (PLUS DE 4 RÉCLAMATIONS)";
+        document.getElementById('btnsoum').disabled = true;
         
     } if (nombreReclamations <= 4){
         errorElement.innerHTML = "";
+        document.getElementById('btnsoum').disabled = false;
     
-    
-    while (detailsDesReclamationsDiv.firstChild) {
-        detailsDesReclamationsDiv.removeChild(detailsDesReclamationsDiv.firstChild);
-    }
+        var nbrenleve = 0;
+        while (detailsDesReclamationsDiv.firstChild) {
+
+            detailsDesReclamationsDiv.removeChild(detailsDesReclamationsDiv.firstChild);
+            
+         
+        }
+          
+
+
+
 
     for (var i = 1; i <= nombreReclamations; i++) {
+        
+
         var label = document.createElement('label');
         label.textContent = 'Pour la réclamation #' + i + ', quel montant avez-vous réclamé?';
         
@@ -91,11 +109,17 @@ function handleNombreReclamationsChange() {
         input.max = '35000'; // On définit une limite de 35 000$ pour le montant de la réclamation
         input.addEventListener('change', handleMontantChange);
         
+        var hauteurnouv = hauteurMtn + 50;
+        tailleFormulaire.style.height = hauteurnouv + 'px';
         detailsDesReclamationsDiv.appendChild(label);
         detailsDesReclamationsDiv.appendChild(input);
         detailsDesReclamationsDiv.appendChild(document.createElement('br'));
+      
     }
 }
+
+
+
 }
 // Cette fonction est appelée lorsque le montant d'une réclamation est modifié
 function handleMontantChange(event) {
@@ -107,8 +131,10 @@ function handleMontantChange(event) {
 
     if (montant > 35000) {
         errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour ce profil de client (PLUS DE 35 000$ DE RÉCLAMATION)";
+        document.getElementById('btnsoum').disabled = true;
     } else {
         errorElement.innerHTML = "";
+        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -124,19 +150,23 @@ function checkAgeAndGender() {
     messageElement.style.backgroundColor = "white";
     
     // a separer les validations ...
-    if ((gender === "femme" && age == 16) || (age == 100) || 
-        ((gender === "homme" || gender === "non-binaire") && age < 18)) {
-        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour ce profil de client (AGE ET GENRE)";
+    if (gender === "femme" && age == 16) {
+        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour une femme de 16 ans.";
+        document.getElementById('btnsoum').disabled = true;  
+    }
+    else if (age == 100) {
+        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour une personne de 100 ans.";
+        document.getElementById('btnsoum').disabled = true;  
 
-        // desactiver les autres champs si jamais on reviens  A REPETER POUR LES AUTRES CHAMPS
-        document.getElementById('valeurv').disabled = true;
-        document.getElementById('anneev').disabled = true;
-
-        // rendre les choses disables icitteeee  A REPETER POUR LES AUTRES CHAMPS
-    } else {
-        document.getElementById('valeurv').disabled = false;
-        document.getElementById('anneev').disabled = false;
+    }
+    else if ((gender === "homme" || gender === "non-binaire") && age < 18) {
+        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour un homme ou une personne non binaire de moins de 18 ans.";
+        document.getElementById('btnsoum').disabled = true;  
+    }
+     else {
+        
         messageElement.innerHTML = "";
+        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -150,13 +180,13 @@ function checkCarValue() {
     // le vide ne marche pas
     if (carValue === "" || carValue == null || isNaN(carValue) ) {
         errorElement.innerHTML = "S'il vous plaît, entrez une valeur pour la voiture";
-        document.getElementById('anneev').disabled = true;
+        document.getElementById('btnsoum').disabled = true;  
     } else if ( carValue > 100000 || carValue < 0) {
         errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour ce profil de client (VALEUR DE LACHAT INVALIDE)";
-        document.getElementById('anneev').disabled = true;
+        document.getElementById('btnsoum').disabled = true;  
     } else {
         errorElement.innerHTML = "";
-        document.getElementById('anneev').disabled = false;
+        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -172,13 +202,17 @@ function checkCarAnnee() {
     // vérifier si le champ est vide
     if (anneeCar === "" || anneeCar == null || isNaN(anneeCar)) {
         errorElement.innerHTML = "S'il vous plaît, entrez une année pour le véhicule";
+        document.getElementById('btnsoum').disabled = true;  
     // vérifier si l'année est plus ancienne de 25 ans
     } else if (currentYear - anneeCar > 25 ) {
         errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour un véhicule de plus de 25 ans.";
+        document.getElementById('btnsoum').disabled = true;  
     // vérifier si l'utilisateur a entré une année future
     } else if (anneeCar > currentYear) {
         errorElement.innerHTML = "Désolé, vous ne pouvez pas entrer une année future.";
+        document.getElementById('btnsoum').disabled = true;  
     } else {
         errorElement.innerHTML = "";
+        document.getElementById('btnsoum').disabled = false;  
     }
 }
