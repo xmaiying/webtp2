e55d76f8d1df591dd5db837e8d82f704fc1bb4d6
MyAnh <myanh_chau@outlook.com>
2023-07-01 02:14:41 -0400

    hauteur
    jai ajuster la hauteur du formulaire pour qu'il peut augmenter et diminuer en mm temps lors de nombre de reclammations qui aug ou dim

--- test.js
+++ test.js
@@ -35,12 +35,13 @@ window.onload = function() {
     document.getElementById('oui').addEventListener('change', handleReclamationChange);
     document.getElementById('non').addEventListener('change', handleReclamationChange);
     document.getElementById('nombreReclamations').addEventListener('input', handleNombreReclamationsChange);
+
+    
     document.getElementById('btnsoum').addEventListener('click', calculerMontant);
 
   
 
 }
-
 
 
 
@@ -60,12 +61,6 @@ function handleReclamationChange() {
 }
 
 
-
-
-
-
-
-
 function handleNombreReclamationsChange() {
     var nombreReclamations = document.getElementById('nombreReclamations').value;
     var detailsDesReclamationsDiv = document.getElementById('details-des-reclamations');
@@ -75,9 +70,6 @@ function handleNombreReclamationsChange() {
     errorElement.style.backgroundColor = "white";
 
     // MODIFICAITON DE LA TAILLE DU FORMULAIRE
-    var tailleFormulaire = document.getElementById('formeassurance');
-    var hauteurMtn = tailleFormulaire.clientHeight;
-    
 
     if (nombreReclamations > 4) {
         errorElement.style.marginBottom = '20px';
@@ -90,11 +82,9 @@ function handleNombreReclamationsChange() {
     
         var nbrenleve = 0;
         while (detailsDesReclamationsDiv.firstChild) {
-
             detailsDesReclamationsDiv.removeChild(detailsDesReclamationsDiv.firstChild);
-            
-         
         }
+        
 
     for (var i = 1; i <= nombreReclamations; i++) {
         
@@ -110,8 +100,7 @@ function handleNombreReclamationsChange() {
         input.max = '35000'; // On définit une limite de 35 000$ pour le montant de la réclamation
         input.addEventListener('change', handleMontantChange);
         
-        var hauteurnouv = hauteurMtn + 50;
-        tailleFormulaire.style.height = hauteurnouv + 'px';
+
         detailsDesReclamationsDiv.appendChild(label);
         detailsDesReclamationsDiv.appendChild(input);
         detailsDesReclamationsDiv.appendChild(document.createElement('br'));
@@ -133,10 +122,8 @@ function handleMontantChange(event) {
 
     if (montant > 35000) {
         errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour ce profil de client (PLUS DE 35 000$ DE RÉCLAMATION)";
-        document.getElementById('btnsoum').disabled = true;
     } else {
         errorElement.innerHTML = "";
-        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -154,21 +141,17 @@ function checkAgeAndGender() {
     // a separer les validations ...
     if (gender === "femme" && age == 16) {
         messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour une femme de 16 ans.";
-        document.getElementById('btnsoum').disabled = true;  
     }
     else if (age == 100) {
-        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour une personne de 100 ans.";
-        document.getElementById('btnsoum').disabled = true;  
+        messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour une personne de 100 ans."; 
 
     }
     else if ((gender === "homme" || gender === "non-binaire") && age < 18) {
         messageElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour un homme ou une personne non binaire de moins de 18 ans.";
-        document.getElementById('btnsoum').disabled = true;  
     }
      else {
         
         messageElement.innerHTML = "";
-        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -181,19 +164,14 @@ function checkCarValue() {
 
     if (carValue === "" || carValue == null) {
         errorElement.innerHTML = "S'il vous plaît, entrez une valeur pour la voiture. Le champ ne peut pas être vide.";
-        document.getElementById('btnsoum').disabled = true;  
     } else if (isNaN(carValue)) {
         errorElement.innerHTML = "La valeur entrée pour la voiture n'est pas un nombre. Veuillez entrer une valeur numérique.";
-        document.getElementById('btnsoum').disabled = true;
     } else if (carValue > 100000) {
         errorElement.innerHTML = "La valeur de la voiture ne peut pas dépasser 100 000. Veuillez entrer une valeur inférieure.";
-        document.getElementById('btnsoum').disabled = true;  
     } else if (carValue < 0) {
-        errorElement.innerHTML = "La valeur de la voiture ne peut pas être négative. Veuillez entrer une valeur supérieure à 0.";
-        document.getElementById('btnsoum').disabled = true;  
+        errorElement.innerHTML = "La valeur de la voiture ne peut pas être négative. Veuillez entrer une valeur supérieure à 0."; 
     } else {
         errorElement.innerHTML = "";
-        document.getElementById('btnsoum').disabled = false;  
     }
 }
 
@@ -210,18 +188,14 @@ function checkCarAnnee() {
     // vérifier si le champ est vide
     if (anneeCar === "" || anneeCar == null || isNaN(anneeCar)) {
         errorElement.innerHTML = "S'il vous plaît, entrez une année pour le véhicule";
-        document.getElementById('btnsoum').disabled = true;  
     // vérifier si l'année est plus ancienne de 25 ans
     } else if (currentYear - anneeCar > 25 ) {
-        errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour un véhicule de plus de 25 ans.";
-        document.getElementById('btnsoum').disabled = true;  
+        errorElement.innerHTML = "Désolé, nous n'avons aucun produit à offrir pour un véhicule de plus de 25 ans.";  
     // vérifier si l'utilisateur a entré une année future
     } else if (anneeCar > currentYear) {
         errorElement.innerHTML = "Désolé, vous ne pouvez pas entrer une année future.";
-        document.getElementById('btnsoum').disabled = true;  
     } else {
         errorElement.innerHTML = "";
-        document.getElementById('btnsoum').disabled = false;  
     }
 }
 function calculerMontant(event) {
@@ -252,12 +226,9 @@ function calculerMontant(event) {
    
     var montantMensuel=assuranceAnnuelle/12;
     afficherMontant.style.color= "red";
-    afficherMontant.style.backgroundcolor= "white";
-    afficherMontant.innerHTML="Le montant annuel est "+assuranceAnnuelle+"et le montant mensuel est "+montantMensuel;
+    afficherMontant.style.backgroundColor = "white";
+    afficherMontant.innerHTML="Le montant annuel est "+assuranceAnnuelle+" et le montant mensuel est "+montantMensuel;
 
-
-
-    
 }
 
 
